20b405f3b1d74f88a2896a885109f23adc40dec2
Jasmine Herrera <jzmnherrera@gmail.com>
2021-06-22 05:13:39 +0000

    Added form to submit contact info

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -13,12 +13,16 @@ public class FormHandlerServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Get the value entered in the form.
-    String textValue = request.getParameter("text-input");
+    String nameValue = request.getParameter("name-input");
+    String emailValue = request.getParameter("email-input");
+    String messValue = request.getParameter("mess-input");
+
 
     // Print the value so you can see it in the server logs.
-    System.out.println("You submitted: " + textValue);
+    System.out.println("User submitted:\nName = " + nameValue + "\nEmail = " + emailValue + "\nMessage = " + messValue);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    response.getWriter().println(
+        "Success!\nYou submitted:\nName = " + nameValue + "\nEmail = " + emailValue + "\nMessage = " + messValue);
   }
 }
